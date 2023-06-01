128e80cb95b6498ee6095b86d7190f7827ae961f
litmus-zhang <abdulsalamlukmon9@gmail.com>
2023-06-01 19:04:33 +0000

    amend the box shadow

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -19,9 +19,11 @@ export default function Reviews() {
   return (
     <div>
       <div className="bg-purple-200 p-10">
-        {
+     <div className="flex flex-wrap justify-center gap-3">
+     {
           REVIEWS.map((review, i) => <Card key={i} {...review} />)
         }
+     </div>
       </div>
     </div>
   )
@@ -37,30 +39,25 @@ interface ReviewProps {
 const Card = (props: ReviewProps) => {
   const { review, reviewer, company, imageUrl } = props;
   return (
-    <div className="relative w-fit ">
-<div className="absolute right-0 bottom-0 bg-black w-[500px] ml-4 -z-1">
-<p>text</p>
-</div>
-      <div className="bg-white w-[500px] p-8">
+    <div className="bg-white w-[500px] p-8 shadow-[16px-16px_rgba(0,0,0,0)]">
+
+      <div className="">
+        <svg width="48" height="32" fill="none"><path d="m20.61 7.93-1-4.73C10.57 3.28 2 7.61 2 19v9.8h13.44V18.53H9.66c0-6.1 2.4-9.95 10.95-10.6Zm6.97 20.87h13.44V18.53h-5.78c0-6.1 2.4-9.95 10.95-10.6l-1-4.73c-9.05.08-17.61 4.41-17.61 15.8v9.8Z" fill="#fff" stroke="#00040F"></path>
+        </svg>
+        <h1 className="font-light text-2xl my-4">{review}</h1>
+
+      </div>
+
+      <div className="flex justify-between items-center">
+        <div className="">
+          <p>{reviewer}</p>
+          <p className="uppercase">{company}</p>
+        </div>
 
         <div className="">
-          <svg width="48" height="32" fill="none"><path d="m20.61 7.93-1-4.73C10.57 3.28 2 7.61 2 19v9.8h13.44V18.53H9.66c0-6.1 2.4-9.95 10.95-10.6Zm6.97 20.87h13.44V18.53h-5.78c0-6.1 2.4-9.95 10.95-10.6l-1-4.73c-9.05.08-17.61 4.41-17.61 15.8v9.8Z" fill="#fff" stroke="#00040F"></path>
-          </svg>
-          <h1 className="font-light text-2xl my-4">{review}</h1>
-
+          <img src={imageUrl} alt="company" />
         </div>
 
-        <div className="flex justify-between items-center">
-          <div className="">
-            <p>{reviewer}</p>
-            <p className="uppercase">{company}</p>
-          </div>
-
-          <div className="">
-            <img src={imageUrl} alt="company" />
-          </div>
-
-        </div>
       </div>
     </div>
   )
